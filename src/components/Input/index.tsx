e8ef349bab8c10520892f03f018ac5ba05b8d337
yuda1124 <yuda1124@naver.com>
2022-08-14 00:23:51 +0900

    feat: input 컴포넌트 구현

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,14 @@
+import style from './index.module.scss'
+
+type InputProps = {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  value: any
+  type: string
+  placeholder?: string
+}
+
+const Input = ({ onChange, value, type, placeholder }: InputProps) => {
+  return <input className={style.container} {...{ onChange, value, type, placeholder }} />
+}
+
+export { Input }

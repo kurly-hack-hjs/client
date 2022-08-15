5ca47b05ed0747abaf6f2f442d113f00e633c4cb
yuda1124 <yuda1124@naver.com>
2022-08-15 16:50:07 +0900

    feat: logistics, user, order 및 API에서 사용될 response, params 타입 정의

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,46 @@ export type Option = {
   label: string
   value: any
 }
+
+export type LogisticsCenter = {
+  name: string
+  code: string
+}
+
+export type GetLogisticsResponse = LogisticsCenter[]
+
+export type Product = {
+  id: number
+  name: string
+  amount: number
+}
+
+export enum ORDER_STATUS {
+  validated = 'validated',
+  not_validated = 'not_validated',
+  canceled = 'canceled',
+  failed = 'failed',
+}
+
+export type Order = {
+  id: number
+  status: ORDER_STATUS
+  products: Product[]
+}
+
+export type GetOrdersResponse = Order[]
+
+export type User = {
+  userName: string
+  code: string
+  logistic_center: LogisticsCenter
+}
+
+export type LoginParams = {
+  userName: string
+  code: string
+  password: string
+  logistic_code: string
+}
+
+export type LoginResponse = User

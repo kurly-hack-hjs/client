71c49b6d4eda48862a53d3cea5c6cbcd7b531b90
yuda1124 <yuda1124@naver.com>
2022-08-15 16:47:40 +0900

    fix: json 함수에 async/await 키워드 추가

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -13,8 +13,8 @@ export const handlers = [
     return res(ctx.status(200), ctx.json(orders))
   }),
 
-  rest.post('/login', (req, res, ctx) => {
-    const { userName, code, password } = req.json()
+  rest.post('/login', async (req, res, ctx) => {
+    const { userName, code, password } = await req.json()
     if (user.userName != userName || user.code != code || user.password != password) {
       return res(
         ctx.status(403),

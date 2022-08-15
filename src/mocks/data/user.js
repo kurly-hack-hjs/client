d24932be47fb151a972286ad8054a3f6ba25f6a9
yuda1124 <yuda1124@naver.com>
2022-08-15 16:06:42 +0900

    feat: handlers에서 data 분리, login mock API 추가

---
+++ src/mocks/data/user.js
@@ -0,0 +1,6 @@
+export default {
+  userName: 'sodam',
+  code: '1234',
+  password: '1234',
+  logistic_center: { name: '김포', code: 'C-03' },
+}
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,99 +1,7 @@
 import { rest } from 'msw'
-
-const logistics = [
-  { label: '송파', value: 'C-01' },
-  { label: '항동', value: 'C-02' },
-  { label: '김포', value: 'C-03' },
-  { label: '화도', value: 'C-04' },
-  { label: '죽전', value: 'C-05' },
-]
-
-const orders = [
-  {
-    id: 0,
-    status: 'validated',
-    products: [
-      {
-        id: 0,
-        name: '[폴바셋] 바리스타 돌체라떼 330ml',
-        amount: 1,
-      },
-      {
-        id: 1,
-        name: '데이&곤약젤리 레몬 깔라만시 제품명...',
-        amount: 2,
-      },
-      {
-        id: 2,
-        name: '[고래사어묵] 김치 우동전골',
-        amount: 3,
-      },
-    ],
-  },
-  {
-    id: 1,
-    status: 'not_validated',
-    products: [
-      {
-        id: 0,
-        name: '[폴바셋] 바리스타 돌체라떼 330ml',
-        amount: 2,
-      },
-      {
-        id: 1,
-        name: '데이&곤약젤리 레몬 깔라만시 제품명...',
-        amount: 1,
-      },
-      {
-        id: 2,
-        name: '[고래사어묵] 김치 우동전골',
-        amount: 1,
-      },
-    ],
-  },
-  {
-    id: 2,
-    status: 'canceled',
-    products: [
-      {
-        id: 0,
-        name: '[폴바셋] 바리스타 돌체라떼 330ml',
-        amount: 3,
-      },
-      {
-        id: 1,
-        name: '데이&곤약젤리 레몬 깔라만시 제품명...',
-        amount: 2,
-      },
-      {
-        id: 2,
-        name: '[고래사어묵] 김치 우동전골',
-        amount: 5,
-      },
-    ],
-  },
-  {
-    id: 3,
-    status: 'failed',
-    products: [
-      {
-        id: 0,
-        name: '[폴바셋] 바리스타 돌체라떼 330ml',
-        amount: 2,
-      },
-      {
-        id: 1,
-        name: '데이&곤약젤리 레몬 깔라만시 제품명...',
-        amount: 1,
-      },
-      {
-        id: 2,
-        name: '[고래사어묵] 김치 우동전골',
-        amount: 1,
-      },
-    ],
-  },
-]
+import orders from './data/orders'
+import logistics from './data/logistics'
+import user from './data/user'
 
 export const handlers = [
   // 물류센터
@@ -103,5 +11,18 @@ export const handlers = [
 
   rest.get('/orders', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(orders))
+  }),
+
+  rest.post('/login', (req, res, ctx) => {
+    const { userName, code, password } = req.json()
+    if (user.userName != userName || user.code != code || user.password != password) {
+      return res(
+        ctx.status(403),
+        ctx.json({
+          errorMessage: 'Failed to log in: username or password are invalid',
+        }),
+      )
+    }
+    return res(ctx.status(200), ctx.json(user))
   }),
 ]

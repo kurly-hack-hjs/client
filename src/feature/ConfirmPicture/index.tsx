6fff61771dba67cc00c43cd0b41e357984dc10bc
yuda1124 <yuda1124@naver.com>
2022-08-17 00:58:52 +0900

    feat: Confirm Picture 피처 추가

---
+++ src/feature/ConfirmPicture/index.tsx
@@ -0,0 +1,63 @@
+import style from './index.module.scss'
+import { Button } from '@src/components'
+
+const ConfirmPicture = () => (
+  <div className={style.container}>
+    <h3 className={style.section_title}>사진 검증</h3>
+    <p className={style.section_desc}>시범 운영모드는 주문당 5번의 사진 검증을 지원합니다.</p>
+    <ul>
+      <li className={`${style.confirm_result} ${style.warning}`}>
+        <div className={style.result_header}>
+          <img className={style.circle}></img>
+          <p>1차 검증 결과</p>
+          <img className={style.icon}></img>
+        </div>
+        <div className={style.result_container}>
+          <div className={style.result_content}></div>
+        </div>
+      </li>
+      <li className={`${style.confirm_result} ${style.success}`}>
+        <div className={style.result_header}>
+          <img className={style.circle}></img>
+          <p>2차 검증 결과</p>
+          <img className={style.icon}></img>
+        </div>
+        <div className={style.result_container}>
+          <div className={style.result_content}></div>
+        </div>
+      </li>
+      <li className={`${style.confirm_result} ${style.before}`}>
+        <div className={style.result_header}>
+          <img className={style.circle}></img>
+          <p>3차 검증 결과</p>
+          <img className={style.icon}></img>
+        </div>
+        <div className={style.result_container}>
+          <Button block>검증하기</Button>
+        </div>
+      </li>
+      <li className={`${style.confirm_result} ${style.before}`}>
+        <div className={style.result_header}>
+          <img className={style.circle}></img>
+          <p>4차 검증 결과</p>
+          <img className={style.icon}></img>
+        </div>
+        <div className={style.result_container}>
+          <Button block>검증하기</Button>
+        </div>
+      </li>
+      <li className={`${style.confirm_result} ${style.before}`}>
+        <div className={style.result_header}>
+          <img className={style.circle}></img>
+          <p>5차 검증 결과</p>
+          <img className={style.icon}></img>
+        </div>
+        <div className={style.result_container}>
+          <Button block>검증하기</Button>
+        </div>
+      </li>
+    </ul>
+  </div>
+)
+
+export { ConfirmPicture }

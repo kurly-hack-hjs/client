dddf583251e77b1510c1a538a4261e3e2ed05ce6
yuda1124 <yuda1124@naver.com>
2022-08-22 02:18:27 +0900

    feat: getScanStatusString service 함수 작성

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,14 @@
-export {}
+import { SCAN_STATUS } from '@src/types'
+
+export const getScanStatusString = (scanStatus: SCAN_STATUS) => {
+  switch (scanStatus) {
+    case SCAN_STATUS.complete:
+      return '검증완료'
+    case SCAN_STATUS.orderCancel:
+      return '검증불필요'
+    case SCAN_STATUS.scanFail:
+      return '재검증필요'
+    case SCAN_STATUS.standby:
+      return '미검증'
+  }
+}

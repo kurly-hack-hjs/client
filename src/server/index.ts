f88c772187c30834a32f5484f09ae57cc63625c9
yuda1124 <yuda1124@naver.com>
2022-08-23 00:09:59 +0900

    chore: change server port

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,7 @@ const appRoot = require('app-root-path')
 const express = require('express')
 
 const app = express()
-const port = process.env.PORT || '8080'
+const port = process.env.PORT || '5000'
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(appRoot.resolve('/build')))

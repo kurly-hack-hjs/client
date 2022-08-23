df2ca11d5fa558fa4a42c350de7249c5ee4954f6
yuda1124 <yuda1124@naver.com>
2022-08-24 03:11:58 +0900

    feat: isLoading atom 추가

--- src/hooks/useGetOrderCallback.tsx
+++ src/hooks/useGetOrderCallback.tsx
@@ -2,6 +2,7 @@ import * as OrderAPI from '@apis/order'
 import orderAtomFamily from '@recoil/order'
 import { GetOrderParams } from '@src/types'
 import { useRecoilCallback } from 'recoil'
+import isLoadingAtom from '@recoil/global'
 
 const useGetOrderCallback = () =>
   useRecoilCallback(
@@ -9,8 +10,15 @@ const useGetOrderCallback = () =>
       async ({ orderId }: GetOrderParams) => {
         const order = await snapshot.getPromise(orderAtomFamily(orderId))
         if (order) return
-        const data = await OrderAPI.getOrder({ orderId })
-        set(orderAtomFamily(orderId), data)
+        set(isLoadingAtom, true)
+        try {
+          const data = await OrderAPI.getOrder({ orderId })
+          set(orderAtomFamily(orderId), data)
+        } catch (e) {
+          console.log(e)
+        } finally {
+          set(isLoadingAtom, false)
+        }
       },
     [orderAtomFamily],
   )
--- src/hooks/useLoginCallback.tsx
+++ src/hooks/useLoginCallback.tsx
@@ -1,18 +1,22 @@
 import * as UserAPI from '@apis/user'
-import { LoginParams } from '@src/types'
+import isLoadingAtom from '@recoil/global'
 import userAtom from '@recoil/user'
-import { useRecoilCallback } from 'recoil'
+import { LoginParams } from '@src/types'
 import { useNavigate } from 'react-router-dom'
+import { useRecoilCallback } from 'recoil'
 
 const useLoginCallback = () => {
   const navigate = useNavigate()
   return useRecoilCallback(({ set }) => async ({ loginId, password, staffCode, centerName }: LoginParams) => {
+    set(isLoadingAtom, true)
     try {
       await UserAPI.login({ loginId, password, staffCode, centerName })
       set(userAtom, { loginId, staffCode, centerName })
       navigate('/list')
     } catch (error) {
       console.log('error in Login')
+    } finally {
+      set(isLoadingAtom, false)
     }
   })
 }

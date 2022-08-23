d694486849cd56e579b12218e78007224a15ccc4
yuda1124 <yuda1124@naver.com>
2022-08-23 21:07:38 +0900

    feat: defaultLayout 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,17 @@ import { Route, Routes } from 'react-router-dom'
 import { Login } from './Login'
 import { OrderList } from './OrderList'
 import { Confirm } from './Confirm'
+import { DefaultLayout } from '@components'
 
 const App = () => (
-  <>
+  <DefaultLayout>
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="login" element={<Login />} />
       <Route path="list" element={<OrderList />} />
       <Route path="confirm/:orderId" element={<Confirm />} />
     </Routes>
-  </>
+  </DefaultLayout>
 )
 
 export default App

6a19723e57eefbd5c5a9df190b1922e65598609e
yuda1124 <yuda1124@naver.com>
2022-08-24 02:13:04 +0900

    feat: SCAN_STATUS 상태 추가

--- src/services/index.ts
+++ src/services/index.ts
@@ -10,13 +10,22 @@ AWS.config.update({
 export const getScanStatusString = (scanStatus: SCAN_STATUS) => {
   switch (scanStatus) {
     case SCAN_STATUS.complete:
-      return '검증완료'
+      return '검증 완료'
     case SCAN_STATUS.orderCancel:
-      return '검증불필요'
+      return '검증 불필요'
     case SCAN_STATUS.scanFail:
-      return '재검증필요'
+      return '재검증 필요'
     case SCAN_STATUS.standby:
       return '미검증'
+    case SCAN_STATUS.scanning:
+      return '검증 진행중'
+    case SCAN_STATUS.reset:
+      return '리셋'
+    case SCAN_STATUS.countError:
+      return '수량 에러'
+    case SCAN_STATUS.etc:
+    default:
+      return '기타'
   }
 }
 

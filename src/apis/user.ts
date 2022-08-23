9877ea5a70e15e215cc5267c759fcff30208b139
yuda1124 <yuda1124@naver.com>
2022-08-23 17:22:26 +0900

    chore: fix api path

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -2,7 +2,7 @@ import endpoint from './endpoint'
 import { LoginParams, LoginResponse } from '@src/types'
 
 const login = async (params: LoginParams): Promise<LoginResponse> => {
-  const response = await endpoint.post('/', params)
+  const response = await endpoint.post('/login', params)
   const { data } = response
   console.log(response)
   return data

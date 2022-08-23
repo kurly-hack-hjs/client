7e373875fed2b8f562fa29e37fd5420f0d521140
yuda1124 <yuda1124@naver.com>
2022-08-23 17:40:59 +0900

    feat: login 후 list 페이지로 라우팅 하는 기능 구현

--- src/hooks/useLoginCallback.tsx
+++ src/hooks/useLoginCallback.tsx
@@ -2,11 +2,19 @@ import * as UserAPI from '@apis/user'
 import { LoginParams } from '@src/types'
 import userAtom from '@recoil/user'
 import { useRecoilCallback } from 'recoil'
+import { useNavigate } from 'react-router-dom'
 
-const useLoginCallback = () =>
-  useRecoilCallback(({ set }) => async ({ loginId, password, staffCode, centerName }: LoginParams) => {
-    const data = await UserAPI.login({ loginId, password, staffCode, centerName })
-    set(userAtom, data)
+const useLoginCallback = () => {
+  const navigate = useNavigate()
+  return useRecoilCallback(({ set }) => async ({ loginId, password, staffCode, centerName }: LoginParams) => {
+    try {
+      await UserAPI.login({ loginId, password, staffCode, centerName })
+      set(userAtom, { loginId, staffCode, centerName })
+      navigate('/list')
+    } catch (error) {
+      console.log('error in Login')
+    }
   })
+}
 
 export { useLoginCallback }
